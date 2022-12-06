baa7a9b9a306cb636dc482856aef7940a720e6a3
Kathryn Szombatfalvy <kathrynszombatfalvy@gmail.com>
2022-12-05 21:48:49 -0600

    tested addToCollection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -6,7 +6,6 @@ function addToCollection(title, artist, yearPublished) {
     return collection.at(-1);
 }
 
-
 // Test addToCollection
 console.log('Just added', addToCollection('Oops!... I Did It Again', 'Britney Spears', 2000));
 console.log('Just added', addToCollection('thank u, next', 'Ariana Grande', 2019));
@@ -14,5 +13,4 @@ console.log('Just added', addToCollection('No More Drama', 'Missy Elliott', 2001
 console.log('Just added', addToCollection('Sing to the Moon', 'Laura Mvula', 2013));
 console.log('Just added', addToCollection('Voodoo', "D'Angelo", 2000));
 console.log('Just added', addToCollection('Iconology', 'Missy Elliott', 2019));
-
 console.log(collection);
