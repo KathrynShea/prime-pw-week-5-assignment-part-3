1e5a50b3735bab080852c0ed666da8802570f605
Kathryn Szombatfalvy <kathrynszombatfalvy@gmail.com>
2022-12-05 21:56:54 -0600

    added and tested showCollection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -6,6 +6,14 @@ function addToCollection(title, artist, yearPublished) {
     return collection.at(-1);
 }
 
+function showCollection(array){
+    console.log(array.length);
+    
+    for(let type of array){
+        console.log(type.title, 'by', type.artist + ', published in', type.yearPublished);
+    }
+}
+
 // Test addToCollection
 console.log('Just added', addToCollection('Oops!... I Did It Again', 'Britney Spears', 2000));
 console.log('Just added', addToCollection('thank u, next', 'Ariana Grande', 2019));
@@ -14,3 +22,6 @@ console.log('Just added', addToCollection('Sing to the Moon', 'Laura Mvula', 201
 console.log('Just added', addToCollection('Voodoo', "D'Angelo", 2000));
 console.log('Just added', addToCollection('Iconology', 'Missy Elliott', 2019));
 console.log(collection);
+
+//Test showCollection
+showCollection(collection);
