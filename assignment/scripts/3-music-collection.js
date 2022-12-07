a75cbb64f8e88a597d62639e50ed40eb74f0f283
Kathryn Szombatfalvy <kathrynszombatfalvy@gmail.com>
2022-12-06 22:07:01 -0600

    add some tracks and search function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,13 +1,13 @@
 console.log("***** Music Collection *****");
 let collection = [];
 
-function addToCollection(title, artist, yearPublished) {
-  collection.push({ title, artist, yearPublished });
+function addToCollection(title, artist, yearPublished, tracks) {
+  collection.push({ title, artist, yearPublished, tracks });
   return collection.at(-1);
 }
 
 function showCollection(array) {
-  console.log(array.length);
+  console.log('There are', array.length, 'items in the collection');
 
   for (let type of array) {
     console.log(
@@ -16,65 +16,98 @@ function showCollection(array) {
       type.artist + ", published in",
       type.yearPublished
     );
+    for (let i = 0; i < 3; i++) {
+      console.log(i + 1, type.tracks[i].name, ":", type.tracks[i].duration);
+    }
   }
 }
 
-function findByArtist(thisArtist){
+function findByArtist(thisArtist) {
+  let albumsByArtist = [];
 
-    let songsByArtist  = [];
-
-    for (let i = 0; i < collection.length; i++){
-        if (collection[i].artist === thisArtist){
-            songsByArtist.push(collection[i]);
-        }
-        
-    } 
-    console.log(songsByArtist.length);
-    return songsByArtist;
+  for (let i = 0; i < collection.length; i++) {
+    if (collection[i].artist === thisArtist) {
+      albumsByArtist.push(collection[i]);
+    }
+  }
+  //console.log(albumsByArtist.length);
+  return albumsByArtist;
 }
 
-function search(searchForName, searchForYear){
-    let newArray = [];
+function search(object) {
+  let newArray = [];
+  //artist and year are not passing over
 
-    for (let i = 0; i < collection.length; i++){
-       if (collection[i].artist === searchForName){
-            if(collection[i].yearPublished === searchForYear){
-                newArray.push(collection[i]);
-            }
-       }
+  //if (artist.empty() || year.empty()) {
+   // return collection;
+//  }
+  for (let i = 0; i < collection.length; i++) {
+    if (collection[i].artist === object.name && collection[i].yearPublished === object.year) {
+        newArray.push(collection[i]);
     }
-
-    return newArray;
+  }
+  return newArray;
 }
 
 // Test addToCollection
 console.log(
   "Just added",
-  addToCollection("Oops!... I Did It Again", "Britney Spears", 2000)
+  addToCollection("Oops!... I Did It Again", "Britney Spears", 2000, [
+    { name: "Opps!...I Did It Again", duration: "3:32" },
+    { name: "Stronger", duration: "3:23" },
+    { name: "Lucky", duration: "3:17" },
+  ])
 );
 console.log(
   "Just added",
-  addToCollection("thank u, next", "Ariana Grande", 2019)
+  addToCollection("thank u, next", "Ariana Grande", 2019, [
+    { name: "imagine", duration: "3:32" },
+    { name: "needy", duration: "2:52" },
+    { name: "NASA", duration: "3:02" },
+  ])
 );
 console.log(
   "Just added",
-  addToCollection("No More Drama", "Missy Elliott", 2001)
+  addToCollection("No More Drama", "Missy Elliott", 2001, [
+    { name: "imagine", duration: "3:32" },
+    { name: "needy", duration: "2:52" },
+    { name: "NASA", duration: "3:02" },
+  ])
 );
 console.log(
   "Just added",
-  addToCollection("Sing to the Moon", "Laura Mvula", 2013)
+  addToCollection("Sing to the Moon", "Laura Mvula", 2013, [
+    { name: "imagine", duration: "3:32" },
+    { name: "needy", duration: "2:52" },
+    { name: "NASA", duration: "3:02" },
+  ])
 );
-console.log("Just added", addToCollection("Voodoo", "D'Angelo", 2000));
-console.log("Just added", addToCollection("Iconology", "Missy Elliott", 2019));
+console.log(
+  "Just added",
+  addToCollection("Voodoo", "D'Angelo", 2000, [
+    { name: "imagine", duration: "3:32" },
+    { name: "needy", duration: "2:52" },
+    { name: "NASA", duration: "3:02" },
+  ])
+);
+console.log(
+  "Just added",
+  addToCollection("Iconology", "Missy Elliott", 2019, [
+    { name: "imagine", duration: "3:32" },
+    { name: "needy", duration: "2:52" },
+    { name: "NASA", duration: "3:02" },
+  ])
+);
 console.log(collection);
 
 //Test showCollection
 showCollection(collection);
 
 //Test findByArtist
-console.log('Songs by Ariana Grande', findByArtist('Ariana Grande'));
-console.log('Songs by Missy Elliott', findByArtist('Missy Elliott'));
-console.log('Songs by Justin Bieber', findByArtist('Justin Bieber'));
+console.log("Albums by Ariana Grande", findByArtist("Ariana Grande"));
+console.log("Albums by Missy Elliott", findByArtist("Missy Elliott"));
+console.log("Albums by Justin Bieber", findByArtist("Justin Bieber"));
 
 //Test search
-console.log("Songs by D'Angelo in 2000", search("D'Angelo", 2000));
+console.log("Albums by Ariana Grande in 2019", search({name: "Ariana Grande", year: 2019}));
+console.log("Albums by empty", search("Ariana Grande"));
