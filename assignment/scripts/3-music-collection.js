c5cc54fa242e986c92741f58f28418716afc297b
Kathryn Szombatfalvy <kathrynszombatfalvy@gmail.com>
2022-12-06 20:13:09 -0600

    added and tested findByArtist

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,27 +1,64 @@
-console.log('***** Music Collection *****')
+console.log("***** Music Collection *****");
 let collection = [];
 
 function addToCollection(title, artist, yearPublished) {
-    collection.push({title, artist, yearPublished});
-    return collection.at(-1);
+  collection.push({ title, artist, yearPublished });
+  return collection.at(-1);
 }
 
-function showCollection(array){
-    console.log(array.length);
-    
-    for(let type of array){
-        console.log(type.title, 'by', type.artist + ', published in', type.yearPublished);
-    }
+function showCollection(array) {
+  console.log(array.length);
+
+  for (let type of array) {
+    console.log(
+      type.title,
+      "by",
+      type.artist + ", published in",
+      type.yearPublished
+    );
+  }
+}
+
+function findByArtist(thisArtist){
+
+    let songsByArtist  = [];
+
+    for (let i = 0; i < collection.length; i++){
+        if (collection[i].artist === thisArtist){
+            songsByArtist.push(collection[i]);
+        }
+        
+    } 
+    console.log(songsByArtist.length);
+    return songsByArtist;
 }
 
 // Test addToCollection
-console.log('Just added', addToCollection('Oops!... I Did It Again', 'Britney Spears', 2000));
-console.log('Just added', addToCollection('thank u, next', 'Ariana Grande', 2019));
-console.log('Just added', addToCollection('No More Drama', 'Missy Elliott', 2001));
-console.log('Just added', addToCollection('Sing to the Moon', 'Laura Mvula', 2013));
-console.log('Just added', addToCollection('Voodoo', "D'Angelo", 2000));
-console.log('Just added', addToCollection('Iconology', 'Missy Elliott', 2019));
+console.log(
+  "Just added",
+  addToCollection("Oops!... I Did It Again", "Britney Spears", 2000)
+);
+console.log(
+  "Just added",
+  addToCollection("thank u, next", "Ariana Grande", 2019)
+);
+console.log(
+  "Just added",
+  addToCollection("No More Drama", "Missy Elliott", 2001)
+);
+console.log(
+  "Just added",
+  addToCollection("Sing to the Moon", "Laura Mvula", 2013)
+);
+console.log("Just added", addToCollection("Voodoo", "D'Angelo", 2000));
+console.log("Just added", addToCollection("Iconology", "Missy Elliott", 2019));
 console.log(collection);
 
 //Test showCollection
 showCollection(collection);
+
+//Test findByArtist
+
+console.log('Songs by Ariana Grande', findByArtist('Ariana Grande'));
+console.log('Songs by Missy Elliott', findByArtist('Missy Elliott'));
+console.log('Songs by Justin Bieber', findByArtist('Justin Bieber'));
